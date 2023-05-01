5c9aa6b78eb765f89af0580a089108dad5f20251
Linber93 <>
2023-05-01 14:59:23 +0000

    add hypothesises and validation results to dashboard

--- app_pages/project_hypothesis.py
+++ app_pages/project_hypothesis.py
@@ -1,10 +1,8 @@
 import streamlit as st
-import os
-from app_pages.data_visualization import image_montage
 
 
 def project_hypothesis():
-    """Content to summarize the project"""
+    """Content to projects hypothesises"""
 
 
 st.write("## Project Hypothesis and Validation")
@@ -15,13 +13,33 @@ st.info(
     f"by white powdery spot on the leaves and stem."
 )
 
-my_data_dir = 'inputs/datasets/cherry-leaves/cherry-leaves'
-labels = os.listdir(my_data_dir + '/validation')
-label_to_display = st.selectbox(label="Select label", options=labels, index=0)
-image_montage(dir_path=my_data_dir + '/validation',
-              label_to_display=label_to_display,
-              nrows=3, ncols=3, figsize=(6, 20))
-
 st.success(
-    f"Looking at"
+    f"The image montage in 'Data visualization' we can confirm that "
+    f"the infected leaves is easily identifiable by the "
+    f"powder white spots on leaves and steam."
+)
+
+st.write("### Hypothesis 2")
+st.info(
+    f"* It can be assumed that a working model could predict if the "
+    f"leaf is infected despite being provided with images "
+    f"in different orientations."
+)
+st.warning(
+    f"I can partially be validated by the model performning well"
+    f" after the images being augmented before being fitted."
+)
+
+st.write("### Hypothesis 3")
+st.info(
+    f"We suspect that mildew could also be detected on other plants and leaves."
+)
+st.error(
+    f"We could NOT validate this hypothesis.\n\n"
+    f"When tried predicting mildew on other plants the model"
+    f"did wrongly predict mildew.\n\n"
+    f"Additionally when the model was provided with and image of something "
+    f"else but a leaf it tried to make a prediction on that too"
+    f"instead of rejecting the input.\n\n"
+    f"Further training required to implement such features "
 )
